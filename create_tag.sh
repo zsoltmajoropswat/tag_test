bede8dfba1a103e12a4b7c6f06a6011773d8f187
Bitbucket Pipeline <bitbucket-pipeline@example.com>
2025-03-17 10:34:57 +0100

    Enhance tag creation script to check for existing tags and generate new ones

--- create_tag.sh
+++ create_tag.sh
@@ -2,12 +2,26 @@
 
 set -e
 
+echo -e "Creating a tag for the current branch...\n"
+
 # Check if there is a tag on the current branch
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-tag=$(git describe --tags --exact-match 2>/dev/null)
+tag=$(git describe --tags --exact-match 2>/dev/null || echo "")
 
-if [ $? -eq 0 ]; then
+DATE=$(date -u +%Y%m%d)
+BASE_TAG="v${DATE}"
+
+# Properly check if a tag exists
+if [ -n "$tag" ]; then
     echo "There is a tag ($tag) on the current branch ($current_branch)."
 else
     echo "There is no tag on the current branch ($current_branch)."
+    COUNT=$(git tag | grep -E "^${BASE_TAG}" | wc -l)
+    NEXT_NUM=$((COUNT + 1))
+    TAG="${BASE_TAG}.${NEXT_NUM}-Community"
+        
+    git config --global user.email "bitbucket-pipeline@example.com"
+    git config --global user.name "Bitbucket Pipeline"
+    
+    echo ${TAG}
 fi
